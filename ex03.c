1a12646fc439dde3f546ec9d1fc624f83ab07d71
Sandro Tributi <sandro.tributi@gmail.com>
2022-10-13 15:37:34 +0200

    initial commit

---
+++ ex03.c
@@ -0,0 +1,24 @@
+/*
+ * Si realizzi un programma in linguaggio C che acquisisca da tastiera un numero
+ * e stampi un messaggio che indichi se tale numero sia positivo oppure negativo.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;    // numero inserito
+
+    // lettura del numero
+    printf("Immetti il numero: ");
+    scanf("%d", &num);
+    // verifica se il numero è positivo o negativo
+    if (num >= 0) {
+        // il numero è positivo o nullo
+        printf("Il numero %d e' POSITIVO\n", num);
+    }
+    else {
+        printf("Il numero %d e' NEGATIVO\n", num);
+    }
+
+    return 0;
+}
